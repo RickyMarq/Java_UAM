f06373d56fdfd1610d51572b8714bc5d5de66825
Henrique Marques <86681672+RickyMarq@users.noreply.github.com>
2022-10-13 21:45:53 -0300

    Add files via upload

---
+++ Aula_13-10/Teste.java
@@ -0,0 +1,36 @@
+package teste;
+import java.util.Scanner;
+import java.lang.String;
+
+
+/**
+ *
+ * @author profslpa
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        
+        
+        
+    }
+
+    public class ContaCorrente {
+        
+        // métodos
+        private int numero;
+        private double saldo;
+        
+        public void setNumero(int numero) {
+            this.numero = numero;
+        }
+    
+    }
+    
+    
+}
+
